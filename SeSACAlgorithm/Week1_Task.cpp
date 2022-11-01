116713f51b8a87f36af1fb824937ea3ea047835f
joongwon <wnddnjs823@ajou.ac.kr>
2022-11-01 16:23:23 +0900

    백준 2161

--- SeSACAlgorithm/Week1_Task.cpp
+++ SeSACAlgorithm/Week1_Task.cpp
@@ -6,19 +6,36 @@
 //
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main(int argc, const char* argv[]) {
     
-    long A, B;
-    cin >> A >> B;
+    int N;
+    cin >> N;
     
-    long result;
+    queue<int> generalQueue;
+    queue<int> discardedQueue;
     
-    result = (A + B) * (A - B);
+    for(int i = 1; i <= N; i++) {
+        generalQueue.push(i);
+    }
     
-    cout << result << "\n";
-        
+    while(generalQueue.size() != 1) {
+        discardedQueue.push(generalQueue.front());
+        generalQueue.pop();
+        generalQueue.push(generalQueue.front());
+        generalQueue.pop();
+    }
+    
+    for(int i = 0; i < discardedQueue.size(); i++) {
+        cout << discardedQueue.front() << " ";
+        discardedQueue.push(discardedQueue.front());
+        discardedQueue.pop();
+    }
+    
+    cout << generalQueue.front() << " ";
+    
     return 0;
 }
