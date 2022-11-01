67e05d15c3e1fc439f562dfa0824f11c29977911
joongwon <wnddnjs823@ajou.ac.kr>
2022-11-01 17:49:33 +0900

    백준 1715

--- SeSACAlgorithm/Week1_Task.cpp
+++ SeSACAlgorithm/Week1_Task.cpp
@@ -5,47 +5,70 @@
 //  Created by 이중원 on 2022/11/01.
 //
 
+
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main(int argc, const char* argv[]) {
+//
+//    //정수가 최대 십만개까지 입력될 수 있기 때문에, 일일히 sort해주면 시간 초과..!! => 다른 자료구조 생각
+//    int arr[100000];
+//    int N;
+//    cin >> N;
+//
+//    for(int i = 1; i <= N; i++) {
+//        int x;
+//        cin >> x;
+//
+//        arr[i - 1] = x;
+//        sort(arr, arr + i);
+//
+//        if(i % 2 == 0) {
+//            cout << arr[i / 2 - 1] << "\n";
+//        } else {
+//            cout << arr[i / 2] << "\n";
+//        }
+//    }
+//
+//    return 0;
+//}
+
 #include <iostream>
-#include <set>
+#include <queue>
 
 using namespace std;
 
 int main(int argc, const char* argv[]) {
     
-    int a, b;
-    cin >> a >> b;
+    int N;
+    cin >> N;
     
-    set<int> A;
-    set<int> B;
+    priority_queue<int, vector<int>, greater<int>> pq;
     
-    int count = 0;
+    int result = 0;
     
-    while(a--) {
-        int x;
-        cin >> x;
+    while(N--) {
+        int num;
+        cin >> num;
         
-        A.insert(x);
+        pq.push(num);
     }
     
-    while(b--) {
-        int x;
-        cin >> x;
+    while(pq.size() != 1) {
+        int first, second;
+        first = pq.top();
+        pq.pop();
+        second = pq.top();
+        pq.pop();
         
-        B.insert(x);
+        result += first + second;
+        pq.push(first + second);
+        
     }
     
-    for(set<int>::iterator iter = B.begin(); iter != B.end(); iter++) {
-        if (A.count(*iter)) {
-            A.erase(*iter);
-            count ++;
-        }
-    }
-    
-    cout << A.size() << "\n";
-    
-    for(set<int>::iterator iter = A.begin(); iter != A.end(); iter++) {
-        cout << *iter << " ";
-    }
+    cout << result << "\n";
     
     return 0;
 }
